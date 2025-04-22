a0331bc2592473fbb1efcccab09b8d38cfd9a8e9
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2025-04-21 19:01:40 -0600

    Add missing database tables and fields

--- airport-db-management/WriteUps/1DatabaseStructure.md
+++ airport-db-management/WriteUps/1DatabaseStructure.md
@@ -15,66 +15,12 @@ CREATE TABLE OrderItems (
 );
 
 ## Database
-``` SQL
-CREATE DATABASE airport;
-
-CREATE TABLE airplane (
-    ssn varchar(9) PRIMARY KEY,
-    reg_number text,
-    model_number text,
-    FOREIGN KEY (model_number) REFERENCES airplane_model(model_number)
-);
-
-CREATE TABLE employee (
-    ssn varchar(9) PRIMARY KEY,
-    name text,
-    password text,
-    address text,
-    phone text,
-    salary numeric(100, 2),
-);
-
-CREATE TABLE expert (
-    -- Composite Key{
-    ssn varchar(9) NOT NULL,
-    model_number text NOT NULL,
-    -- }Composite Key
-    PRIMARY KEY (ssn, model_number),
-);
-
-CREATE TABLE airplane_model (
-    model_number text PRIMARY KEY,
-    capacity int,
-    weight int,
-);
-
-CREATE TABLE employee (
-    ssn varchar(9) PRIMARY KEY,
-);
-
-CREATE TABLE test_event (
-    -- Composite Key{
-    test_number int NOT NULL,
-    ssn varchar(9) NOT NULL,
-    reg_number text NOT NULL,
-    test_date date NOT NULL,
-    -- }Composite Key
-    duration int,
-    score int,
-    PRIMARY KEY (test_number, ssn, reg_number, test_date),
-);
-
-CREATE TABLE faa_test (
-    test_number int PRIMARY KEY,
-    name text,
-    max_score int,
-);
-
-CREATE TABLE technician (
-    ssn varchar(9) PRIMARY KEY,
-);
-
-CREATE TABLE atc (
-    ssn varchar(9) PRIMARY KEY,
-);
-```
+- employee
+- technician
+- atc
+- expert
+- airplane_model
+- airplane
+- test_event
+- faa_test
+- manager
--- airport-db-management/create_database.sql
+++ airport-db-management/create_database.sql
@@ -1,77 +1,89 @@
 CREATE DATABASE airport;
 
+
+-- Plane Tables --
+CREATE TABLE faa_test (
+    test_number INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    max_score INT,
+);
+
 CREATE TABLE airplane (
-    ssn varchar(9) PRIMARY KEY,
-    reg_number text,
-    model_number text,
+    reg_number TEXT PRIMARY KEY,
+    model_number TEXT NOT NULL,
     FOREIGN KEY (model_number) REFERENCES airplane_model(model_number)
 );
 
-CREATE TABLE employee (
-    ssn varchar(9) PRIMARY KEY,
-    name text,
-    password text,
-    address text,
-    phone text,
-    salary numeric(100, 2),
-);
-
-CREATE TABLE expert (
-    -- Composite Key{
-    ssn varchar(9) NOT NULL,
-    model_number text NOT NULL,
-    -- }Composite Key
-    PRIMARY KEY (ssn, model_number),
-);
-
 CREATE TABLE airplane_model (
-    model_number text PRIMARY KEY,
-    capacity int,
-    weight int,
-);
-
-CREATE TABLE employee (
-    ssn varchar(9) PRIMARY KEY,
+    model_number TEXT PRIMARY KEY,
+    capacity INT CHECK (capacity > 0),
+    weight INT CHECK (weight > 0),
 );
 
 CREATE TABLE test_event (
-    -- Composite Key{
-    test_number int NOT NULL,
-    ssn varchar(9) NOT NULL,
-    reg_number text NOT NULL,
-    test_date date NOT NULL,
-    -- }Composite Key
-    duration int,
-    score int,
-    PRIMARY KEY (test_number, ssn, reg_number, test_date),
+    test_number INT NOT NULL,
+    ssn VARCHAR(9) NOT NULL,
+    reg_number TEXT NOT NULL,
+    date DATE NOT NULL,
+    duration INT CHECK (duration > 0),
+    score INT CHECK,
+    PRIMARY KEY (test_number, ssn, reg_number, date),
+    FOREIGN KEY (test_number) REFERENCES faa_test(test_number),
+    FOREIGN KEY (ssn) REFERENCES expert(ssn),
+    FOREIGN KEY (reg_number) REFERENCES airplane(reg_number)
 );
 
-CREATE TABLE faa_test (
-    test_number int PRIMARY KEY,
-    name text,
-    max_score int,
+
+-- Employee Tables --
+CREATE TABLE employee (
+    ssn VARCHAR(9) PRIMARY KEY,
+    name TEXT NOT NULL,
+    password TEXT,
+    address TEXT,
+    phone TEXT,
+    salary numeric(100, 2) CHECK (salary > 0),
 );
 
+-- Inherits from employee
+CREATE TABLE manager (
+    ssn VARCHAR(9) PRIMARY KEY,
+    FOREIGN KEY (ssn) REFERENCES employee(ssn)
+);
+
+-- Inherits from employee
 CREATE TABLE technician (
-    ssn varchar(9) PRIMARY KEY,
+    ssn VARCHAR(9) PRIMARY KEY,
+    FOREIGN KEY (ssn) REFERENCES employee(ssn)
 );
 
+-- Inherits from technician
+CREATE TABLE expert (
+    ssn VARCHAR(9) NOT NULL,
+    model_number TEXT NOT NULL,
+    PRIMARY KEY (ssn, model_number),
+    FOREIGN KEY (model_number) REFERENCES airplane_model(model_number),
+    FOREIGN KEY (ssn) REFERENCES technician(ssn)
+);
+
+-- Inherits from employee
 CREATE TABLE atc (
-    ssn varchar(9) PRIMARY KEY,
+    ssn VARCHAR(9) PRIMARY KEY,
+    FOREIGN KEY (ssn) REFERENCES employee(ssn)
 );
 
 
+-- Procedures --
 CREATE PROCEDURE InsertTestEvent(
-    IN p_test_name text,
-    IN p_test_date date
+    IN p_test_name TEXT,
+    IN p_date DATE
 )
 AS
 BEGIN
-    IF p_test_date > CURDATE() THEN
+    IF p_date > CURDATE() THEN
         SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'test_date cannot be in the future';
+        SET MESSAGE_TEXT = 'date cannot be in the future';
     ELSE
-        INSERT INTO test_event (test_name, test_date)
-        VALUES (p_test_name, p_test_date);
+        INSERT INTO test_event (test_name, date)
+        VALUES (p_test_name, p_date);
     END IF;
 END;
